9daefcada64c40f032b2f8e79c47e0c729e05690
Monisha-Chauhan <113276443+Monisha-Chauhan@users.noreply.github.com>
2024-03-27 21:55:29 +0530

    Create Path Sum.cpp

---
+++ Trees 3/Path Sum.cpp
@@ -0,0 +1,10 @@
+ bool hasPathSum(TreeNode* root, int targetSum) {
+       if(root==NULL)return false; 
+   
+    if(!root->left && !root->right)
+       return targetSum==root->val;
+      
+       bool left=hasPathSum(root->left,targetSum - root->val);
+       bool right=hasPathSum(root->right,targetSum - root->val);
+       return left||right;
+    }
