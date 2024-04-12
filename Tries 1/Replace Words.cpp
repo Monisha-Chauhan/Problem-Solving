5d3b0c4419c194f0eb417f6d85f2766d33912128
Monisha-Chauhan <113276443+Monisha-Chauhan@users.noreply.github.com>
2024-04-12 22:56:00 +0530

    Create Replace Words.cpp

---
+++ Tries 1/Replace Words.cpp
@@ -0,0 +1,80 @@
+class Solution {
+public:
+ struct Node {
+        Node * child[26];
+        bool flag = false;
+        Node()
+        {
+            for( int i=0;i<26;i++)
+                 child[i]=NULL;
+        }
+        bool isContains(char ch){      
+            return child[ch-'a'] != NULL;
+        }
+        void put (char ch, Node * node){
+            child[ch-'a'] = node;
+        }
+        Node* get(char ch){
+            return child[ch-'a'];
+        }
+    };
+    class Trie {
+    private : Node * root;
+    public:
+        Trie() {
+            root = new Node();
+        }
+          void insert(string word) {
+            Node * node = root;
+            int n = word.size();
+            for(int i = 0;i<n;i++){
+                if(!(node ->isContains(word[i]))){      
+                    node->put(word[i],new Node());
+                }
+                node = node->get(word[i]);        
+            }
+            node->flag = true;   
+
+        }
+         string Transform(string sentence) {
+            string word , ans = "";
+            stringstream sobj;
+            sobj<<sentence;
+            while(!sobj.eof()){ 
+                getline(sobj,word,' ');    
+                string rootWord = "";
+                Node * node = root;
+                int n = word.size();
+                int f = 0;
+                for(int i = 0;i<n;i++){
+                    if(node->isContains(word[i])){
+                        rootWord.push_back(word[i]);          
+                        node = node->get(word[i]);
+                        if(node->flag){   
+                            if(ans.size()>0) ans.push_back(' ');
+                            ans += rootWord;
+                            f= 1;
+                            break;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                if(!f){        
+                    if(ans.size()>0) ans.push_back(' ');
+                    ans += word;
+                }
+                
+            }
+            return ans;
+        }
+
+    };
+    string replaceWords(vector<string>& dictionary, string sentence) {
+       Trie trie;
+        for(auto  word : dictionary) trie.insert(word);
+        string ans = trie.Transform(sentence);
+        return ans;  
+    }
+};
