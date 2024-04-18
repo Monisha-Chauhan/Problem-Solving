84eee599f3b5c823465cd1ced352106fe7dc0088
Monisha-Chauhan <113276443+Monisha-Chauhan@users.noreply.github.com>
2024-04-18 21:59:30 +0530

    Create Minimum Number of Vertices to Reach All Nodes.cpp

---
+++ Graph 1/Minimum Number of Vertices to Reach All Nodes.cpp
@@ -0,0 +1,16 @@
+vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+        vector<bool> indegree(n,false);
+        for(auto i:edges)
+        {
+            int u=i[0];
+            int v=i[1];
+            indegree[v]=true;
+        }
+        vector<int> ans;
+        for(int i=0;i<n;i++)
+        {
+            if(indegree[i]==false)
+            ans.push_back(i);
+        }
+        return ans;
+    }
