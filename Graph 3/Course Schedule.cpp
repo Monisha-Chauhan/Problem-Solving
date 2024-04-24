68b88e60adc33860eaab8ce86675f42be3785411
Monisha-Chauhan <113276443+Monisha-Chauhan@users.noreply.github.com>
2024-04-24 21:59:19 +0530

    Create Course Schedule.cpp

---
+++ Graph 3/Course Schedule.cpp
@@ -0,0 +1,31 @@
+ bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+     vector<int> adj[numCourses];
+     for(auto i:prerequisites)
+       adj[i[0]].push_back(i[1]);
+     vector<int> indegree(numCourses,0);
+     for(int i=0;i<numCourses;i++)
+     {
+        for(auto it:adj[i])
+           indegree[it]++;
+     }   
+     queue<int> q;
+     for(int i=0;i<numCourses;i++)
+     {
+        if(indegree[i]==0)
+          q.push(i);
+     }
+     vector<int> topo;
+     while(!q.empty())
+     {
+        int node=q.front();
+        q.pop();
+        topo.push_back(node);
+        for(auto i:adj[node])
+        {
+            indegree[i]--;
+            if(indegree[i]==0)q.push(i);
+        }
+     }
+     if(topo.size()==numCourses)return true;
+      return false;
+    }
